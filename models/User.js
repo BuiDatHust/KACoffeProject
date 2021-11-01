e41c4bdb492886f629f44955cc7db60750c65c20
Nguyenlthoe <nguyenlthoe@gmail.com>
2021-11-01 17:47:21 +0700

    add field password

--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const user = new Schema({
-    name: {type: String, require: true, maxlength: 100,},
+    name: {type: String, require: true, maxLength: 100,},
     phonenumber: {type: Number, require: true},
     defautAddress: {type: String, require: false},
+    password: {type: String, require: true, minLength: 8},
 })
 module.exports = mongoose.model('user', user);
