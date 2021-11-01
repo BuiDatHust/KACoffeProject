5067e6facfb32414c2d2f9c5d756a75654edf944
Phạm Văn Linh <linh.pv194094@sis.hust.edu.vn>
2021-11-01 17:07:01 +0700

    update module product

--- models/Product.js
+++ models/Product.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Product = new Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
-    Image: { type: String, required: true },
+    name: { type: String, required: [ true, 'Please provide name field' ], maxLength: [ 50, "Name can not be more than 50 character" ] },
+    price: { type: Number, required: [ true, 'Please provide price field' ] },
+    description: { type: String, required: [ true, 'Please provide description field' ], maxLength: [ 200, "Description can not be more than 200 character" ]  },
+    Image: { type: String, required: [ true, 'Please provide link image' ] },
     averageRating: { type: Number, default: 0 },
     numOfReviews: { type: Number, default: 0 },
-    user: { type: String, required: true },
+    user: { type: mongoose.Types.ObjectId, ref: User, required: true },
 })
 
 module.exports = mongoose.model('products', Product)
