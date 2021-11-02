1659359f836d40edfe9c193a30edfd0344a1c2b9
hoangtc125 <anhhoangday.69@gmail.com>
2021-11-02 16:35:16 +0700

    update model: discount, review

--- models/Discount.js
+++ models/Discount.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const discount = new Schema({
-    minusPrice: { type: Number, default: 0 },
-    apply: { type: Date, }
+    minusPrice: { type: Number, required: true, default: 0 },
+    apply: { type: String, required: true, enum: ['sliver', 'gold', 'bronze'], }
 })
 module.exports = mongoose.model('discount', discount);
--- models/Review.js
+++ models/Review.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const review = new Schema({
-    rating: { type: Number, require: true, default: 0 },
-    title: { type: String, require: true, maxLength: 200, minLength: 1, },
-    comment: { type: String, require: true, maxLength: 200, minLength: 1, },
+    rating: { type: Number, required: true, default: 0 },
+    title: { type: String, required: true, maxLength: 200, minLength: 1, },
+    comment: { type: String, required: true, maxLength: 200, minLength: 1, },
     user: { type: mongoose.Types.ObjectId, ref: User, required: true },
     product: { type: mongoose.Types.ObjectId, ref: Product, required: true },
 })
