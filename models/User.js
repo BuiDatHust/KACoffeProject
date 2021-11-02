a640723500fe62f78b5f5fedf1baaf7b99418d80
Nguyenlthoe <nguyenlthoe@gmail.com>
2021-11-02 22:15:40 +0700

    Add role, email, orderStatus

--- models/User.js
+++ models/User.js
@@ -8,6 +8,12 @@ const user = new Schema({
     defautAddress: {type: String, require: false},
     password: {type: String, require: true, minLength: 8},
     voucher: [mongoose.Types.ObjectId],
-    tyoeOfMember:{type: String, require: true}
+    rank: {type: String, require: true},
+    email: {type:String, require: true},
+    role: {type: Boolean, require: true},
+    orderStatus: {
+        type: String,
+        Order: mongoose.Types.ObjectId,
+    },
 })
 module.exports = mongoose.model('user', user);
