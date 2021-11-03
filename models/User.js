5d92f328e96c1fca4bc92bc2fd12b886e75333e9
Nguyenlthoe <nguyenlthoe@gmail.com>
2021-11-03 07:48:20 +0700

    add User model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema();
 const user = new Schema({
     name: {type: String, require: true, maxLength: 100,},
     phonenumber: {type: Number, require: true},
-    defautAddress: {type: String, require: false},
+    //defautAddress: {type: String, require: false},
     password: {type: String, require: true, minLength: 8},
     voucher: [mongoose.Types.ObjectId],
     rank: {type: String, require: true},
