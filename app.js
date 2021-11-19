0e8f1f2cfb0c268530fb92e7e8665f4faf549dd1
BuiDatHust <dinhtuan95qh@gmail.com>
2021-11-20 02:02:31 +0700

    add app

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cookieParser('jwtSecret'))
 
 app.use(express.static('./public'));
 app.use(fileUpload());
+app.use(express.urlencoded({extended:false}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
