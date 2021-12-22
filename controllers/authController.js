c9abd2ea184fb23cdba0ae58c99f540fc9e2ab8a
hoangtc125 <anhhoangday.69@gmail.com>
2021-12-22 11:06:04 +0700

    clear cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,74 +1,76 @@
 const User = require('../models/User')
-const {  StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 const BadRequestError = require('../errors/badRequestError')
 const UnauthentiatedError = require('../errors/unauthenticatedError')
-const { attachTokenToRes,createTokenUser } = require('../utils')
+const { attachTokenToRes, createTokenUser } = require('../utils')
 const nodemailer = require('nodemailer')
 const randomString = require('randomstring')
 
-const register = async (req,res) =>{
-    const { email,name,password,phone } = req.body;
+const register = async(req, res) => {
+    const { email, name, password, phone } = req.body;
 
-    const emailExists= await User.findOne({ email })
-    if( emailExists ){
-        res.render('auth/register', { 
-            user: req.user, 
-            warning: 'Email đã được sử dụng, vui lòng thử email khác!' })
+    const emailExists = await User.findOne({ email })
+    if (emailExists) {
+        res.render('auth/register', {
+            user: req.user,
+            warning: 'Email đã được sử dụng, vui lòng thử email khác!'
+        })
         return
     }
 
-    const isFirstAccount = (await User.countDocuments({}) ) ==0
+    const isFirstAccount = (await User.countDocuments({})) == 0
     const role = isFirstAccount ? 'admin' : 'user'
 
-    const user = await User.create({ name,email,password,role,phone })
+    const user = await User.create({ name, email, password, role, phone })
     await user.save()
+    res.clearCookie('token')
     const tokenUser = createTokenUser(user)
-    attachTokenToRes({res,user: tokenUser})
-    res.status(StatusCodes.CREATED).render('index',{user: tokenUser});
+    attachTokenToRes({ res, user: tokenUser })
+    res.status(StatusCodes.CREATED).render('index', { user: tokenUser });
 
 }
 
-const login = async (req,res) =>{
-    const { email,password } = req.body 
-    if( !email || !password ) {
+const login = async(req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
         res.render('auth/login', { user: req.user, warning: 'Vui lòng nhập email hoặc mật khẩu!' })
         return
     }
 
-    user = await User.findOne({email}) 
-    if( !user ) {
+    user = await User.findOne({ email })
+    if (!user) {
         res.render('auth/login', { user: req.user, warning: 'Tài khoản không tồn tại!' })
         return
     }
     const isPasswordCorrect = await user.comparePassword(password)
-    if( !isPasswordCorrect ) {
+    if (!isPasswordCorrect) {
         res.render('auth/login', { user: req.user, warning: 'Mật khẩu không đúng!' })
         return
     }
     const tokenUser = createTokenUser(user)
-    attachTokenToRes({res, user: tokenUser})
-    res.status(StatusCodes.OK).render('index', {user: tokenUser})
+    attachTokenToRes({ res, user: tokenUser })
+    res.status(StatusCodes.OK).render('index', { user: tokenUser })
 }
 
-const logout = async (req, res) => {
+const logout = async(req, res) => {
     // res.cookie('token', 'logout', {
     //   httpOnly: true,
     //   expires: new Date(Date.now() + 500),
     // }); 
     res.clearCookie('token')
     res.status(StatusCodes.OK).redirect('/KACoffe/v1/');
-  };
+};
 
-const forgotPassword = async (req,res) => {
+const forgotPassword = async(req, res) => {
     const email = req.body.email;
     const newPassword = randomString.generate(8)
     console.log(newPassword)
-    if(!email) {
+    if (!email) {
         res.render('auth/forgot-password', { user: req.user, warning: 'Vui lòng nhập email!' })
         return
     }
     user = await User.findOne({ email })
-    if(!user) {
+    if (!user) {
         res.render('auth/forgot-password', { user: req.user, warning: 'Không tồn tại tài khoản với email này!' })
         return
     }
@@ -85,10 +87,10 @@ const forgotPassword = async (req,res) => {
     const mailOptions = {
         from: '"KaCoffee" <ka.coffee.hust@outlook.com>',
         to: email,
-        subject: "Thiết lập lại mật khẩu!", 
-        html: "<b>Mật khẩu mới: </b>" + newPassword, 
+        subject: "Thiết lập lại mật khẩu!",
+        html: "<b>Mật khẩu mới: </b>" + newPassword,
     }
-    transporter.sendMail(mailOptions, function(err,info) {
+    transporter.sendMail(mailOptions, function(err, info) {
         if (err) {
             console.log(err)
             return;
@@ -98,4 +100,4 @@ const forgotPassword = async (req,res) => {
     res.status(StatusCodes.OK).redirect('/KACoffe/v1/auth')
 }
 
-module.exports = { register,login, logout, forgotPassword }
+module.exports = { register, login, logout, forgotPassword }
