7f4e905f77f2971ac83ed755d817d4af330593db
Phạm Văn Linh <linh.pv194094@sis.hust.edu.vn>
2021-12-23 13:34:12 +0700

    update index alert

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -121,13 +121,29 @@ const buyNotLogin = async (req,res) =>{
   var { phone,name,amount,address,nameproduct,email,size } = req.body
   var subtotal=0, total =0
 
+  let productNew = await Product.find({}).sort({ _id: -1 })
+  let caPhe = await Product.find({ category: 'Cà phê' }).sort({ _id: -1 })
+  let traSua = await Product.find({ category: 'Trà trái cây-Trà sữa' }).sort({ _id: -1 })
+  let daXay = await Product.find({ category: 'Đá xay-Choco-Matcha'}).sort({ _id: -1 })
+  let doUongNhanh = await Product.find({ category: 'Đồ uống nhanh'}).sort({ _id: -1 })
+  let drinks = await Product.find({ category: 'Drinks'}).sort({ _id: -1 })
+
   let product = await Product.findOne({name: nameproduct})
 
   if( !product ){
-    return res.render('index', { user:'',status: "Không tìm thấy sản phẩm này" });
+    res.render('index', { 
+    user:'',
+    productNew: productNew,
+    caPhe: caPhe,
+    traSua: traSua,
+    daXay: daXay,
+    doUongNhanh: doUongNhanh,
+    drinks, drinks,
+    status: "Không tìm thấy sản phẩm này" });
+    return
   }
   orderItems = [{
-  name: name,
+  name: nameproduct,
   price: product.price,
   amount: amount,
   size:size,
@@ -145,7 +161,7 @@ const buyNotLogin = async (req,res) =>{
   });
   const mailOptions = {
     from: '"KaCoffee" <ka.coffee.hust@outlook.com>',
-    to: req.body.emai,
+    to: req.body.email,
     subject: "Chúc mừng bạn có đơn hàng đầu tiên", 
     html: "<b>Chúc mừng bạn có đơn hàng đầu tiên: </b> Hãy đăng nhập để có thể hưởng thêm ưu đãi" , 
   }
@@ -167,7 +183,24 @@ phone
 })
 
 
-res.render('index', { user:'',status: "Đặt hàng thành công" });
+
+productNew = productNew.slice(0, 3)
+caPhe = caPhe.slice(0, 3)
+traSua = traSua.slice(0, 3)
+daXay = daXay.slice(0, 3)
+drinks = drinks.slice(0, 3)
+doUongNhanh = doUongNhanh.slice(0, 3)
+console.log(caPhe, traSua, daXay, drinks);
+
+res.render('index', { 
+  user:'',
+  productNew: productNew,
+  caPhe: caPhe,
+  traSua: traSua,
+  daXay: daXay,
+  doUongNhanh: doUongNhanh,
+  drinks, drinks,
+  status: "Đặt hàng thành công" });
 }
 
 const buy = async (req,res) =>{
@@ -392,11 +425,18 @@ const requestToDeleteOrder = async (req,res) =>{
 const getDetailOrder = async (req,res) =>{
   const { id } = req.params
   const order = await Order.findOne({ _id: id })
+  const user = await User.findOne({_id: req.user.userId })
 
-  console.log(order)
+  console.log(order);
 
-  res.render('detailorder',
-  { user: req.user, orderid: order.id.slice(18, 24), orderItems: order.orderItems, subtotal: order.subtotal, address:order.address,status: order.status })
+  res.render('detailorder', { 
+    user: req.user, 
+    email: order.phone,
+    orderid: order.id.slice(18, 24), 
+    orderItems: order.orderItems, 
+    subtotal: order.subtotal, 
+    address:order.address,
+    status: order.status })
 }
 
 const checkAccount = async (req,res) =>{
